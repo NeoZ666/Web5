7a3f976f190c7719029cbc084dda94e31131dc01
neoz666 <neoz.blockchain@gmail.com>
2024-01-07 19:29:25 +0530

    added blob and image support : TEXT works, but Image doesnt.

--- webapp/app/page.js
+++ webapp/app/page.js
@@ -1,10 +1,18 @@
 "use client";
 
+// Import necessary Next.js modules
+import { useState } from 'react';
+import { useRouter } from 'next/router';
 import { Web5 } from "@web5/api";
+import 
 
 export default function Home() {
+  const [file, setFile] = useState(null);
+  const [image, setImage] = useState(null);
+  // const [text, setText] = useState(null);
+  // const router = useRouter();
 
-  const NiluDid = "did:ion:EiCwShHnOLZcJk7eh6Tk19JHRDb4qAqDSpuwdLGIkV8YvQ:eyJkZWx0YSI6eyJwYXRjaGVzIjpbeyJhY3Rpb24iOiJyZXBsYWNlIiwiZG9jdW1lbnQiOnsicHVibGljS2V5cyI6W3siaWQiOiJkd24tc2lnIiwicHVibGljS2V5SndrIjp7ImNydiI6IkVkMjU1MTkiLCJrdHkiOiJPS1AiLCJ4IjoibC1Pb2ZWZDV6QmEyejV4cWhSdW1EQ0F3a2JqT1UtNlJRaWxuaTRqekhIMCJ9LCJwdXJwb3NlcyI6WyJhdXRoZW50aWNhdGlvbiJdLCJ0eXBlIjoiSnNvbldlYktleTIwMjAifSx7ImlkIjoiZHduLWVuYyIsInB1YmxpY0tleUp3ayI6eyJjcnYiOiJzZWNwMjU2azEiLCJrdHkiOiJFQyIsIngiOiJxSWRDc2EzajRrcV92eW5BaDdCUmJybk9INEpOR0ZFQk15eEFLQlh0V2swIiwieSI6IjZlRldYbmFtbVl2bXNYeXhUSGVHZl9HTjhGUm5RdFo1N3NydE1KSnZwblUifSwicHVycG9zZXMiOlsia2V5QWdyZWVtZW50Il0sInR5cGUiOiJKc29uV2ViS2V5MjAyMCJ9XSwic2VydmljZXMiOlt7ImlkIjoiZHduIiwic2VydmljZUVuZHBvaW50Ijp7ImVuY3J5cHRpb25LZXlzIjpbIiNkd24tZW5jIl0sIm5vZGVzIjpbImh0dHBzOi8vZHduLnRiZGRldi5vcmcvZHduNCIsImh0dHBzOi8vZHduLnRiZGRldi5vcmcvZHduNSJdLCJzaWduaW5nS2V5cyI6WyIjZHduLXNpZyJdfSwidHlwZSI6IkRlY2VudHJhbGl6ZWRXZWJOb2RlIn1dfX1dLCJ1cGRhdGVDb21taXRtZW50IjoiRWlCY3FIZXBVb2xMQkNyWDN6bndGTmxpQzRRd3dJMWJFS3hjUHNyUEZHQ1BJZyJ9LCJzdWZmaXhEYXRhIjp7ImRlbHRhSGFzaCI6IkVpRFh2TEtLRWswei0xY21xYThuRFhDRndGTzN1ODFRekpvdlRZenlIOF9fT1EiLCJyZWNvdmVyeUNvbW1pdG1lbnQiOiJFaUJtb2ozRTA4TGozbGkzT1lGdVQ1UUFEZlIxa3ZCNzMycEEwOEVjUnpQUnBRIn19"
+  const NiluDid = "did:ion:EiCwShHnOLZcJk7eh6Tk19JHRDb4qAqDSpuwdLGIkV8YvQ:eyJkZWx0YSI6eyJwYXRjaGVzIjpbeyJhY3Rpb24iOiJyZXBsYWNlIiwiZG9jdW1lbnQiOnsicHVibGljS2V5cyI6W3siaWQiOiJkd24tc2lnIiwicHVibGljS2V5SndrIjp7ImNydiI6IkVkMjU1MTkiLCJrdHkiOiJPS1AiLCJ4IjoibC1Pb2ZWZDV6QmEyejV4cWhSdW1EQ0F3a2JqT1UtNlJRaWxuaTRqekhIMCJ9LCJwdXJwb3NlcyI6WyJhdXRoZW50aWNhdGlvbiJdLCJ0eXBlIjoiSnNvbldlYktleTIwMjAifSx7ImlkIjoiZHduLWVuYyIsInB1YmxpY0tleUp3ayI6eyJjcnYiOiJzZWNwMjU2azEiLCJrdHkiOiJFQyIsIngiOiJxSWRDc2EzajRrcV92eW5BaDdCUmJybk9INEpOR0ZFQk15eEFLQlh0V2swIiwieSI6IjZlRldYbmFtbVl2bXNYeXhUSGVHZl9HTjhGUm5RdFo1N3NydE1KSnZwblUifSwicHVycG9zZXMiOlsia2V5QWdyZWVtZW50Il0sInR5cGUiOiJKc29uV2ViS2V5MjAyMCJ9XSwic2VydmljZXMiOlt7ImlkIjoiZHduIiwic2VydmljZUVuZHBvaW50Ijp7ImVuY3J5cHRpb25LZXlzIjpbIiNkd24tZW5jIl0sIm5vZGVzIjpbImh0dHBzOi8vZHduLnRiZGRldi5vcmcvZHduNCIsImh0dHBzOi8vZHduLnRiZGRldi5vcmcvZHduNSJdLCJzaWduaW5nS2V5cyI6WyIjZHduLXNpZyJdfSwidHlwZSI6IkRlY2VudHJhbGl6ZWRXZWJOb2RlIn1dfX1dLCJ1cGRhdGVDb21taXRtZW50IjoiRWlCY3FIZXBVb2xMQkNyWDN6bndGTmxpQzRRd3dJMWJFS3hjUHNyUEZHQ1BJZyJ9LCJzdWZmaXhEYXRhIjp7ImRlbHRhSGFzaCI6IkVpRFh2TEtLRWswei0xY21xYThuRFhDRndGTzN1ODFRekpvdlRZenlIOF9fT1EiLCJyZWNvdmVyeUNvbW1pdG1lbnQiOiJFaUJtb2ozRTA4TGozbGkzT1lGdVQ1UUFEZlIxa3ZCNzMycEEwOEVjUnpQUnBRIn19";
 
   const configuration = async () => {
     const { web5, did } = await Web5.connect();
@@ -30,7 +38,7 @@ export default function Home() {
               "Song": {
                 "schema": "https://sollertia/protocol/Song",
                 "dataFormats": [
-                  "text/plain"
+                  "application/octet-stream"
                 ]
               },
               "canAccess": {
@@ -71,40 +79,93 @@ export default function Home() {
       },
     });
     const definition = protocol.definition;
+    console.log("PROTOCOL DEFINITION : ", definition);
 
-    const { record: postRecord, status: createStatus } = await web5.dwn.records.create({
-      data: 'Hey this is my first post!',
-      message: {
-        protocol: "https://sollertia/protocol",
-        protocolPath: 'canAccess',
-        recipient: NiluDid,
-        schema: "https://sollertia/protocol/canAccess",
-        dataFormat: 'text/plain'
-      }
-    });
+  //   const { record: postRecord, status: createStatus } = await web5.dwn.records.create({
+  //     data: 'Hey this is my first post!',
+  //     message: {
+  //       protocol: "https://sollertia/protocol",
+  //       protocolPath: 'canAccess',
+  //       recipient: NiluDid,
+  //       schema: "https://sollertia/protocol/canAccess",
+  //       dataFormat: 'text/plain'
+  //     }
+  //   });
 
-    console.log("POST RECORD : ", postRecord);
-    // this creates a record and stores it in the user's local DWN
-    const replyResponse = await web5.dwn.records.create(  {
-      "data": "You can access the song now",
-      "message": {
-        "recipient": "did:ion:EiCwShHnOLZcJk7eh6Tk19JHRDb4qAqDSpuwdLGIkV8YvQ:eyJkZWx0YSI6eyJwYXRjaGVzIjpbeyJhY3Rpb24iOiJyZXBsYWNlIiwiZG9jdW1lbnQiOnsicHVibGljS2V5cyI6W3siaWQiOiJkd24tc2lnIiwicHVibGljS2V5SndrIjp7ImNydiI6IkVkMjU1MTkiLCJrdHkiOiJPS1AiLCJ4IjoibC1Pb2ZWZDV6QmEyejV4cWhSdW1EQ0F3a2JqT1UtNlJRaWxuaTRqekhIMCJ9LCJwdXJwb3NlcyI6WyJhdXRoZW50aWNhdGlvbiJdLCJ0eXBlIjoiSnNvbldlYktleTIwMjAifSx7ImlkIjoiZHduLWVuYyIsInB1YmxpY0tleUp3ayI6eyJjcnYiOiJzZWNwMjU2azEiLCJrdHkiOiJFQyIsIngiOiJxSWRDc2EzajRrcV92eW5BaDdCUmJybk9INEpOR0ZFQk15eEFLQlh0V2swIiwieSI6IjZlRldYbmFtbVl2bXNYeXhUSGVHZl9HTjhGUm5RdFo1N3NydE1KSnZwblUifSwicHVycG9zZXMiOlsia2V5QWdyZWVtZW50Il0sInR5cGUiOiJKc29uV2ViS2V5MjAyMCJ9XSwic2VydmljZXMiOlt7ImlkIjoiZHduIiwic2VydmljZUVuZHBvaW50Ijp7ImVuY3J5cHRpb25LZXlzIjpbIiNkd24tZW5jIl0sIm5vZGVzIjpbImh0dHBzOi8vZHduLnRiZGRldi5vcmcvZHduNCIsImh0dHBzOi8vZHduLnRiZGRldi5vcmcvZHduNSJdLCJzaWduaW5nS2V5cyI6WyIjZHduLXNpZyJdfSwidHlwZSI6IkRlY2VudHJhbGl6ZWRXZWJOb2RlIn1dfX1dLCJ1cGRhdGVDb21taXRtZW50IjoiRWlCY3FIZXBVb2xMQkNyWDN6bndGTmxpQzRRd3dJMWJFS3hjUHNyUEZHQ1BJZyJ9LCJzdWZmaXhEYXRhIjp7ImRlbHRhSGFzaCI6IkVpRFh2TEtLRWswei0xY21xYThuRFhDRndGTzN1ODFRekpvdlRZenlIOF9fT1EiLCJyZWNvdmVyeUNvbW1pdG1lbnQiOiJFaUJtb2ozRTA4TGozbGkzT1lGdVQ1UUFEZlIxa3ZCNzMycEEwOEVjUnpQUnBRIn19",
-        "contextId": "bafyreic4flpi2a346n6i25rd4rfjyjsga2njf4ognsfalu3scc3jblfx2u",
-        "parentId": "bafyreic4flpi2a346n6i25rd4rfjyjsga2njf4ognsfalu3scc3jblfx2u",
-        "protocol": "https://sollertia/protocol",
-        "protocolPath": "Song/canAccess",
-        "schema": "https://sollertia/protocol/canAccess",
-        "dataFormat": "text/plain"
-      }
+  //   console.log("POST RECORD : ", postRecord);
+  //   // this creates a record and stores it in the user's local DWN
+  //   const replyResponse = await web5.dwn.records.create(  {
+  //     "data": "You can access the song now",
+  //     "message": {
+  //       "recipient": "did:ion:EiCwShHnOLZcJk7eh6Tk19JHRDb4qAqDSpuwdLGIkV8YvQ:eyJkZWx0YSI6eyJwYXRjaGVzIjpbeyJhY3Rpb24iOiJyZXBsYWNlIiwiZG9jdW1lbnQiOnsicHVibGljS2V5cyI6W3siaWQiOiJkd24tc2lnIiwicHVibGljS2V5SndrIjp7ImNydiI6IkVkMjU1MTkiLCJrdHkiOiJPS1AiLCJ4IjoibC1Pb2ZWZDV6QmEyejV4cWhSdW1EQ0F3a2JqT1UtNlJRaWxuaTRqekhIMCJ9LCJwdXJwb3NlcyI6WyJhdXRoZW50aWNhdGlvbiJdLCJ0eXBlIjoiSnNvbldlYktleTIwMjAifSx7ImlkIjoiZHduLWVuYyIsInB1YmxpY0tleUp3ayI6eyJjcnYiOiJzZWNwMjU2azEiLCJrdHkiOiJFQyIsIngiOiJxSWRDc2EzajRrcV92eW5BaDdCUmJybk9INEpOR0ZFQk15eEFLQlh0V2swIiwieSI6IjZlRldYbmFtbVl2bXNYeXhUSGVHZl9HTjhGUm5RdFo1N3NydE1KSnZwblUifSwicHVycG9zZXMiOlsia2V5QWdyZWVtZW50Il0sInR5cGUiOiJKc29uV2ViS2V5MjAyMCJ9XSwic2VydmljZXMiOlt7ImlkIjoiZHduIiwic2VydmljZUVuZHBvaW50Ijp7ImVuY3J5cHRpb25LZXlzIjpbIiNkd24tZW5jIl0sIm5vZGVzIjpbImh0dHBzOi8vZHduLnRiZGRldi5vcmcvZHduNCIsImh0dHBzOi8vZHduLnRiZGRldi5vcmcvZHduNSJdLCJzaWduaW5nS2V5cyI6WyIjZHduLXNpZyJdfSwidHlwZSI6IkRlY2VudHJhbGl6ZWRXZWJOb2RlIn1dfX1dLCJ1cGRhdGVDb21taXRtZW50IjoiRWlCY3FIZXBVb2xMQkNyWDN6bndGTmxpQzRRd3dJMWJFS3hjUHNyUEZHQ1BJZyJ9LCJzdWZmaXhEYXRhIjp7ImRlbHRhSGFzaCI6IkVpRFh2TEtLRWswei0xY21xYThuRFhDRndGTzN1ODFRekpvdlRZenlIOF9fT1EiLCJyZWNvdmVyeUNvbW1pdG1lbnQiOiJFaUJtb2ozRTA4TGozbGkzT1lGdVQ1UUFEZlIxa3ZCNzMycEEwOEVjUnpQUnBRIn19",
+  //       "contextId": "bafyreic4flpi2a346n6i25rd4rfjyjsga2njf4ognsfalu3scc3jblfx2u",
+  //       "parentId": "bafyreic4flpi2a346n6i25rd4rfjyjsga2njf4ognsfalu3scc3jblfx2u",
+  //       "protocol": "https://sollertia/protocol",
+  //       "protocolPath": "Song/canAccess",
+  //       "schema": "https://sollertia/protocol/canAccess",
+  //       "dataFormat": "text/plain"
+  //     }
+  //   }
+  //   );
+  // console.log("REPLY RESPONSE : ", replyResponse);
+
+    // Check if a file is selected
+    if (!file) {
+      console.error('No file selected');
+      return;
     }
-    );
-  console.log("REPLY RESPONSE : ", replyResponse);
+
+    // Upload the file
+    const blob = new Blob(file, { type: "image/png" });
+    const { record } = await web5.dwn.records.create({
+      data: blob,
+      message: {
+          dataFormat: "image/png"
+      }
+  });
+
+      console.log("RECORD : ", record);
+      console.log("RECORD ID : ", record._recordId);
+
+    // Continue with the rest of your configuration logic...
+      // let { record } = await web5.dwn.records.read({
+      //   message: {
+      //     filter: {
+      //       recordId: "bafyreibmpu4sinoumlhxolbquzni4sbxnbgr6qin2s4jido6noctdpbmva",
+      //     },
+      //   },
+      // });
+      // console.log("yaha tak hua hai");
+      // // assuming the record has a text payload
+      // const text = await record.data.text();
+      // setImage(text)
+      // console.log("Record Data Text:", text);
+  }
+
+  // Function to handle file input change
+  const handleFileChange = (event) => {
+    const selectedFile = event.target.files && event.target.files[0];
+    setFile(selectedFile);
+    console.log("fileSet:", selectedFile);
   };
+
 
   return (
     <>
       <h1 className="mb-10">Web5 React Example</h1>
-      <button onClick={configuration}>Connect to the Web5 Network</button>
+      <form>
+        <label>
+          Upload File:
+          <input type="file" onChange={handleFileChange} />
+        </label>
+        <button type="button" onClick={configuration}>
+          Connect to the Web5 Network
+        </button>
+        <br />
+        <div>
+          <img src={`data:image/jpeg;base64,${btoa(String.fromCharCode.apply(null, image))}`} alt="Uploaded Image" />
+        </div>
+      </form>
     </>
   );
 }
