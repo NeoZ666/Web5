4578eeefacc164a74572206c794ea203e9120536
Shashwat Tripathi <2021.shashwat.tripathi@ves.ac.in>
2024-01-09 16:13:01 +0530

    Metadata corrected

--- webapp/app/layout.js
+++ webapp/app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "SOLLETRIA",
+  title: "SOLLERTIA",
   description: "Get back your rights and be free",
 };
 
