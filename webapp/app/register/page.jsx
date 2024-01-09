c8f05401efec013feeba031522ccf1c2a5f12258
neoz666 <neoz.blockchain@gmail.com>
2024-01-09 18:59:59 +0530

    bcjaegbaw

--- webapp/app/register/page.jsx
+++ webapp/app/register/page.jsx
@@ -3,15 +3,30 @@
 // import Link from "next/link";
 import React, { useState } from "react";
 import toast from "react-hot-toast";
+import loginProtocol from '../JSONFiles/protocol'
+import useWeb5Initializer from '../utils/web5Initializer';
+import blob from 'blob';
 
 const SignUp = () => {
+
+  console.log("loginProtocol:", ...[loginProtocol]);
+
+  const { web5, did } = useWeb5Initializer();
+  const configure = async () => {
+  const { protocol, status } = await web5.dwn.protocols.configure({
+    message: {
+      definition: loginProtocol
+    }
+  });
+
+  console.log("protocol:", protocol);
+  }
   // const navigate = useNavigate();
 
   const [formData, setFormData] = useState({
     name: "",
     email: "",
     walletAddress: "",
-    password: "",
     role: "user",
     file: "",
   });
@@ -31,19 +46,83 @@ const SignUp = () => {
     formDataToSend.append("name", formData.name);
     formDataToSend.append("email", formData.email);
     formDataToSend.append("walletAddress", formData.walletAddress);
-    formDataToSend.append("password", formData.password);
     formDataToSend.append("role", formData.role);
     formDataToSend.append("file", formData.file); // Append file
 
-    const requestOptions = {
-      method: "POST",
-      body: formDataToSend, // Send FormData object for file upload
-    };
+    const today = new Date();
+    const tomorrow = new Date(today);
+    tomorrow.setDate(today.getDate() + 1);
+    const formattedDate = tomorrow.toISOString().replace(/\.\d{3}Z$/, '.000000Z');
+
+
+    // Create a mixed record
+    async function createMessage({formDataToSend}) {
+
+      const img = new Blob([blob], formDataToSend.file, { type: "image/png" })
+      console.log("img:", img);
+
+      const { record, status } = await web5.dwn.records.create({
+        data: {
+          image: img,
+          name: formDataToSend.name,
+          email: formDataToSend.email,
+          wallet: formDataToSend.walletAddress,
+          role: formDataToSend.role,
+        },
+        message: {
+          protocol: loginProtocol.protocol,
+          dataFormat: "application/json",
+          published: true,
+          schema: "https://sollertia.com/Loginprotocol/user",
+          datePublished: formattedDate.toString({ smallestUnit: 'microseconds' }),
+        }
+      });
+      console.log("record:", record);
+      console.log("status:", status);
+    
+
+    const { record : signupRecord } = await web5.dwn.records.read({
+      message: {
+        filter: {
+          recordId: record._recordId
+        },
+      },
+    });
+
+    
+  
+    // assuming the record has a text payload
+    const text = await signupRecord.data.blob();
+    console.log("text:", ...[text]);
+  }
+    // try {
+    //   const image = new Image();
+    //   const imageUrl = URL.createObjectURL(signupRecord.data.blob());
+    //   img.src = imageUrl;
+    // } catch (err) {
+    //   console.error('Error:', err);
+    // }
+    
+
+
+    toast.promise(createMessage({formDataToSend}), {
+      loading: "Creating your account...",
+      success: (status) => {
+        return "Data Sent Successfully: ", status;
+      },
+      error: (err) => {
+        console.log(err);
+        return err.message;
+      },
+    });
   }
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-900">
       <div className="bg-opacity-25 bg-blur w-96 p-8 rounded-md shadow-md">
+        {/* <div className="imagine">
+          <img src={imageUrl} alt="Thenga" />
+        </div> */}
         <h3 className="text-3xl text-center font-bold text-white mb-4 md:text-5xl">Sign Up</h3>
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
@@ -112,22 +191,6 @@ const SignUp = () => {
             />
           </div>
 
-          <div className="mb-4">
-            <label
-              htmlFor="password"
-              className="block text-sm font-medium text-white"
-            >
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              value={formData.password}
-              onChange={handleChange}
-              className="text-black mt-1 p-2 w-full border rounded-md"
-            />
-          </div>
           <div className="mb-4">
             <label
               htmlFor="role"
