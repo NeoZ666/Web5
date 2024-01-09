10f36e09b57fb4230298c1b86a306d75e27f784e
neoz666 <neoz.blockchain@gmail.com>
2024-01-09 13:16:54 +0530

    uploadProtocol DONE

--- webapp/app/JSONFiles/protocol.js
+++ webapp/app/JSONFiles/protocol.js
@@ -94,82 +94,94 @@ const uploadProtocol = {
     "protocol": "https://sollertia.com/uploadProtocol",
     "published": true,
     "types": {
-        "song": {
-            "schema": "https://sollertia.com/uploadProtocol/song",
-            "dataFormats": [
-                "application/json"
-            ]
-        },
-        "songFile": { // IMAGE OF SONG
-            "schema": "https://sollertia.com/uploadProtocol/songImage",
-            "dataFormats": [
-                "image/png",
-                "image/jpeg",
-                "image/gif"
-            ]
-        },
-        "songName": {
-            "schema": "https://sollertia.com/uploadProtocol/songName",
-            "dataFormats": [
-              "plain/text"
-            ]
-        },
-        "songDesc": {
-            "schema": "https://sollertia.com/uploadProtocol/songDesc",
-            "dataFormats": [
-              "plain/text"
-            ]
-        },
-        "songTrack": { // AUDIO FILE
-            "schema": "https://sollertia.com/uploadProtocol/songTrack",
-            "dataFormats": [
-                "audio/mpeg"
-            ]
-        },
-        "songArtist": {
-            "schema": "https://sollertia.com/uploadProtocol/songArtist",
-            "dataFormats": [
-              "plain/text"
-            ]
-        },
-        "company" : {
-            "schema": "https://sollertia.com/uploadProtocol/company",
-            "dataFormats": [
-                "application/json"
-            ]
-        },
-        "admin" : {
-            "schema": "https://sollertia.com/uploadProtocol/admin",
-            "dataFormats": [
-                "application/json"
-            ]
-        },
+      "song": {
+        "schema": "https://sollertia.com/uploadProtocol/song",
+        "dataFormats": ["application/json"]
+      },
+      "songFile": {
+        "schema": "https://sollertia.com/uploadProtocol/songImage",
+        "dataFormats": ["image/png", "image/jpeg", "image/gif"]
+      },
+      "songName": {
+        "schema": "https://sollertia.com/uploadProtocol/songName",
+        "dataFormats": ["plain/text"]
+      },
+      "songDesc": {
+        "schema": "https://sollertia.com/uploadProtocol/songDesc",
+        "dataFormats": ["plain/text"]
+      },
+      "songTrack": {
+        "schema": "https://sollertia.com/uploadProtocol/songTrack",
+        "dataFormats": ["audio/mpeg"]
+      },
+      "songArtist": {
+        "schema": "https://sollertia.com/uploadProtocol/songArtist",
+        "dataFormats": ["plain/text"]
+      },
+      "company": {
+        "schema": "https://sollertia.com/uploadProtocol/company",
+        "dataFormats": ["application/json"]
+      },
+      "admin": {
+        "schema": "https://sollertia.com/uploadProtocol/admin",
+        "dataFormats": ["application/json"]
+      }
     },
     "structure": {
-        admin: {$globalRole: true},
-        song: {
-            "company": {
-                $actions: [
-                    { who: "author", of: "song", can: "write" },
-                    { who: "author", of: "song", can: "delete" },
-                    { who: "anyone", can: "read"},
-                    { role: "admin", can: "read" },
-                    { role: "admin", can: "delete" }
-                ]
-            },
-            sondTrack: {
-                $actions: [
-                    { role: "song/company", can: "read" },
-                    { role: "song/company", can: "query" },
-                    { role: "author", of: "song", can: "read"  },
-                    { role: "author", of: "song", can: "write"  },
-
-                    { role : "admin", can: "query" },
-                    { role: "admin", can: "read" },
-                    { role: "admin", can: "delete" }
-
-                ]
-            }
+      "admin": { "$globalRole": true },
+      "song": {
+        "company": {
+          "$contextRole": true,
+          "$actions": [
+            { "who": "author", "of": "song", "can": "write" },
+            { "who": "author", "of": "song", "can": "delete" },
+            { "who": "anyone", "can": "read" },
+            { "role": "admin", "can": "read" },
+            { "role": "admin", "can": "delete" }
+          ]
+        },
+        "songTrack": {
+          "$actions": [
+            { "role": "song/company", "can": "read" },
+            { "role": "song/company", "can": "query" },
+            { "who": "author", "of": "song", "can": "read" },
+            { "who": "author", "of": "song", "can": "write" },
+            { "role": "admin", "can": "query" },
+            { "role": "admin", "can": "read" }
+          ]
+        },
+        "songName": {
+          "$actions": [
+            { "role": "song/company", "can": "read" },
+            { "role": "song/company", "can": "query" },
+            { "who": "author", "of": "song", "can": "read" },
+            { "who": "author", "of": "song", "can": "write" },
+            { "role": "admin", "can": "query" },
+            { "role": "admin", "can": "read" }
+          ]
+        },
+        "songDesc": {
+          "$actions": [
+            { "role": "song/company", "can": "read" },
+            { "role": "song/company", "can": "query" },
+            { "who": "author", "of": "song", "can": "read" },
+            { "who": "author", "of": "song", "can": "write" },
+            { "role": "admin", "can": "query" },
+            { "role": "admin", "can": "read" }
+          ]
+        },
+        "songFile": {
+          "$actions": [
+            { "role": "song/company", "can": "read" },
+            { "role": "song/company", "can": "query" },
+            { "who": "author", "of": "song", "can": "read" },
+            { "who": "author", "of": "song", "can": "write" },
+            { "role": "admin", "can": "query" },
+            { "role": "admin", "can": "read" }
+          ]
         }
+      }
     }
-}
+  }
+  
+  
