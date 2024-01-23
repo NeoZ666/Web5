80436a9074d65ab64bbaf022170104450803691d
Shashwat Tripathi <2021.shashwat.tripathi@ves.ac.in>
2024-01-23 09:14:36 +0530

    Added Script in LayoutJS

--- webapp/app/layout.js
+++ webapp/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +12,31 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      {/* <!-- Google tag (gtag.js) --> */}
+      {/* <script async src="https://www.googletagmanager.com/gtag/js?id=G-FC04VSG9S8"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-FC04VSG9S8');
+      </script> */}
+
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-FC04VSG9S8"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-FC04VSG9S8');
+        `}
+      </Script>
       <body className={`${inter.className} bg-darkGray text-slate-200`}>
+        {/* <div>hellu</div> */}
         {children}
       </body>
     </html>
